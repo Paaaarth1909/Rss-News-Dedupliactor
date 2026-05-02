5fa5f55c34c7564b5ed219a82ead7445c9137d40
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-02 20:17:07 +0530

    Core Logic

---
+++ backend/utils/deduplicate.js
@@ -0,0 +1,48 @@
+function cleanText(text) {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s]/g, "")
+    .split(" ")
+    .filter(word => word.length > 2);
+}
+
+function jaccardSimilarity(a, b) {
+  const setA = new Set(a);
+  const setB = new Set(b);
+
+  const intersection = new Set([...setA].filter(x => setB.has(x)));
+  const union = new Set([...setA, ...setB]);
+
+  return intersection.size / union.size;
+}
+
+function clusterArticles(articles) {
+  const clusters = [];
+  const threshold = 0.4;
+
+  for (let article of articles) {
+    let added = false;
+
+    const wordsA = cleanText(article.title);
+
+    for (let cluster of clusters) {
+      const wordsB = cleanText(cluster[0].title);
+
+      const similarity = jaccardSimilarity(wordsA, wordsB);
+
+      if (similarity > threshold) {
+        cluster.push(article);
+        added = true;
+        break;
+      }
+    }
+
+    if (!added) {
+      clusters.push([article]);
+    }
+  }
+
+  return clusters;
+}
+
+module.exports = { clusterArticles };
